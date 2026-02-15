480bb389a59df92c66a65e51adb43a1801f4dd50
silkthreadboutiqueofficial-commits <silkthreadboutiqueofficial@gmail.com>
2026-02-15 21:41:10 +0530

    Add files via upload

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,16 +1,3 @@
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
+import { defineCloudflareConfig, OpenNextConfig  } from "@opennextjs/cloudflare";
 
-const config: OpenNextConfig = {
-  default: {
-    override: {
-      config: {
-        // Mark sharp as external so it's not bundled
-        experimental: {
-          serverComponentsExternalPackages: ["sharp"],
-        },
-      },
-    },
-  },
-};
-
-export default config;
+export default defineCloudflareConfig();
